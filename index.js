fb04ee49f5e5e67b598e80b95eda2756d4631697
hannanuddin78 <hannanuddin78@gmail.com>
2021-01-31 22:26:08 +0600

    code update optaion codeing in metarial degien

--- index.js
+++ index.js
@@ -6,9 +6,8 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(express.static("public"));
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
@@ -23,32 +22,47 @@ const client = new MongoClient(uri, {
 });
 
 client.connect((err) => {
+  // create mongoDB collection when use another design system BootStrap
   const buyCollectionBootStrap = client.db("FinexGateway").collection("buyFinexDataWithBootStrap");
+  const sellCollectionBootStrap = client.db("FinexGateway").collection("sellFinexDataWithBootStrap");
+
+  // optaional test create mongoDB collection when use another design system MATERIAL-UI UI but i not finish sell page
   const buyCollectionMaterial = client.db("FinexGateway").collection("buyFinexDataWithMaterial");
+  const sellCollectionMaterial = client.db("FinexGateway").collection("sellFinexDataWithMaterial");
 
   console.log("database connect");
 
   app.post("/buyCoinDataWithBootStrap", (req, res) => {
-    const event = req.body;
-    console.log(event);
-    buyCollectionBootStrap.insertOne(event).then((result) => {
-      console.log(result.insertedCount);
+    const buyData = req.body;
+    buyCollectionBootStrap.insertOne(buyData).then((result) => {
       res.send(result.insertedCount > 0);
     });
   });
 
+  app.post("/sellCoinDataWithBootStrap", (req, res) => {
+    sellCollectionBootStrap.insertOne(req.body).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+  });
+
+  // optaional test create api when post buy and sell data when use another design system MATERIAL-UI but i not finish sell page
+
   app.post("/buyCoinDataWithMaterial", (req, res) => {
-    const event = req.body;
-    console.log(event);
-    buyCollectionMaterial.insertOne(event).then((result) => {
-      console.log(result.insertedCount);
+    const buyData = req.body;
+    buyCollectionMaterial.insertOne(buyData).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+  });
+
+  app.post("/sellCoinDataWithMaterial", (req, res) => {
+    sellCollectionMaterial.insertOne(req.body).then((result) => {
       res.send(result.insertedCount > 0);
     });
   });
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello World...db connect");
+  res.send("Hello World...server connect successfully");
 });
 
 app.listen(process.env.PORT || port);
